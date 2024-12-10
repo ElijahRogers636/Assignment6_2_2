3236df4b64d823dfbdec948af8d24ca1dc0c0826
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-10 15:52:49 -0500

    commit stuff

--- Program.cs
+++ Program.cs
@@ -14,13 +14,16 @@
         /// <summary>
         /// A * B = B * A
         /// A * (B * C) = (A * B) * C = (A * C) * B
+        /// EXPLAINATION Start: Each time we multiply prod with array values and add to prodHolder we are multiplying all numbers execpt for 1 value needed for program solution
+        /// As we move through the array multiplying all elements together, one way we get closer to finding the product of the final index
+        /// We then pass back through in reverse to find the remaining products for each index
+        /// EXPLAINATION End: Then we multiply the intial products with the remaining products and get our result
         /// The main trick for this problem is to multiply the ends of the array by 1.
         /// We want to create an array to hold our individual products (prodHolder)
         /// We want to set a variable to 1 and use it to hold our products (prod)
         /// Initially loop through the original array.
         /// Each iteration we will set the current index of "prodHolder" to "prod" (our current calculated product)
         /// Then set "prod" to our current "prod" value times current index element of the current array(prod * arr[i])
-        /// EXPLAINATION: Each time we multiply prod with array values and add to prodHolder we are multiplying all numbers execpt for 1 value needed for program solution
         /// Set "prod" to 1 again
         /// Loop the length of the array again, this time in reverse
         /// set current index(prodHolder[i]) equal to "prod" * prodHolder[i](current index element)
